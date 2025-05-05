a80f85a8698ca35c57382703b708733458f85d71
kiyo9w <ngokapikapi@gmail.com>
2025-05-05 16:11:46 +0700

    feat(lexer): enhance token parsing and error handling for unterminated strings and invalid tokens. Enhanced number parsing to support hexadecimal, binary, and octal.

--- development/MODULES/lexer/main.cpp
+++ development/MODULES/lexer/main.cpp
@@ -6,72 +6,33 @@
 #include <iomanip>  // For std::quoted
 #include "../common/token.h"
 #include "lexer.h" // Include Lexer class definition
-#include <map>
-#include "../common/utils.h"
+#include "../common/utils.h" // Include the header for tokenTypeToString declaration
 
 // Print token information in a readable format
 void printToken(const Token& token) {
-    // Map of token types to string representations
-    static const char* tokenTypeStrings[] = {
-        "GARDEN", "SPECIES", "OPEN", "HIDDEN", "GUARDED", 
-        "GROW", "BLOSSOM", "STYLE", "BLOOM", "WATER",
-        "BRANCH", "ELSE", "WHILE", "FOR", "IDENTIFIER",
-        "NUMBER", "STRING", "TRUE", "FALSE", "PLUS",
-        "MINUS", "STAR", "SLASH", "ASSIGN", "EQUAL",
-        "NOT_EQUAL", "LESS", "LESS_EQUAL", "GREATER", "GREATER_EQUAL",
-        "AND", "OR", "NOT", "MODULO", "STREAM_OUT",
-        "STREAM_IN", "ARROW", "LEFT_PAREN", "RIGHT_PAREN", "LEFT_BRACE",
-        "RIGHT_BRACE", "LEFT_BRACKET", "RIGHT_BRACKET", "COMMA", "SEMICOLON",
-        "DOT", "COLON", "COMMENT", "EOF_TOKEN", "ERROR", "STYLE_INCLUDE", "SCOPE_RESOLUTION"
-    };
-    
-    // Get index based on token type (safely)
-    int index = static_cast<int>(token.type);
-    const char* typeStr = (index >= 0 && index < sizeof(tokenTypeStrings)/sizeof(tokenTypeStrings[0])) 
-                         ? tokenTypeStrings[index] : "UNKNOWN";
-    
-    std::cout << "Token: " << typeStr;
-    
-    // Print lexeme for identifiers, numbers, and strings
-    if (token.type == TokenType::IDENTIFIER || 
-        token.type == TokenType::NUMBER || 
-        token.type == TokenType::STRING) {
-        std::cout << " (\"" << token.lexeme << "\")";
-    }
-    
-    std::cout << " at line " << token.line << ", column " << token.column << std::endl;
-}
+    // Use the shared tokenTypeToString from utils.h
+    std::string typeStr = tokenTypeToString(token.type);
 
-// Function to convert TokenType enum to string (reverse of parser's map)
-// Keep this in sync with common/token.h
-std::string tokenTypeToString(TokenType type) {
-    // This map should contain all entries from the TokenType enum
-     static const std::map<TokenType, std::string> typeToStringMap = {
-        {TokenType::GARDEN, "GARDEN"}, {TokenType::SPECIES, "SPECIES"}, {TokenType::OPEN, "OPEN"}, 
-        {TokenType::HIDDEN, "HIDDEN"}, {TokenType::GUARDED, "GUARDED"}, {TokenType::GROW, "GROW"}, 
-        {TokenType::BLOSSOM, "BLOSSOM"}, {TokenType::STYLE, "STYLE"}, {TokenType::BLOOM, "BLOOM"}, 
-        {TokenType::WATER, "WATER"}, {TokenType::BRANCH, "BRANCH"}, {TokenType::ELSE, "ELSE"}, 
-        {TokenType::WHILE, "WHILE"}, {TokenType::FOR, "FOR"}, {TokenType::IDENTIFIER, "IDENTIFIER"},
-        {TokenType::NUMBER, "NUMBER"}, {TokenType::STRING, "STRING"}, {TokenType::TRUE, "TRUE"}, 
-        {TokenType::FALSE, "FALSE"}, {TokenType::PLUS, "PLUS"}, {TokenType::MINUS, "MINUS"}, 
-        {TokenType::STAR, "STAR"}, {TokenType::SLASH, "SLASH"}, {TokenType::ASSIGN, "ASSIGN"}, 
-        {TokenType::EQUAL, "EQUAL"}, {TokenType::NOT_EQUAL, "NOT_EQUAL"}, {TokenType::LESS, "LESS"}, 
-        {TokenType::LESS_EQUAL, "LESS_EQUAL"}, {TokenType::GREATER, "GREATER"}, {TokenType::GREATER_EQUAL, "GREATER_EQUAL"},
-        {TokenType::AND, "AND"}, {TokenType::OR, "OR"}, {TokenType::NOT, "NOT"}, 
-        {TokenType::MODULO, "MODULO"}, {TokenType::STREAM_OUT, "STREAM_OUT"}, {TokenType::STREAM_IN, "STREAM_IN"}, 
-        {TokenType::ARROW, "ARROW"}, {TokenType::LEFT_PAREN, "LEFT_PAREN"}, {TokenType::RIGHT_PAREN, "RIGHT_PAREN"}, 
-        {TokenType::LEFT_BRACE, "LEFT_BRACE"}, {TokenType::RIGHT_BRACE, "RIGHT_BRACE"}, {TokenType::LEFT_BRACKET, "LEFT_BRACKET"}, 
-        {TokenType::RIGHT_BRACKET, "RIGHT_BRACKET"}, {TokenType::COMMA, "COMMA"}, {TokenType::SEMICOLON, "SEMICOLON"},
-        {TokenType::DOT, "DOT"}, {TokenType::COLON, "COLON"}, {TokenType::COMMENT, "COMMENT"},
-        {TokenType::EOF_TOKEN, "EOF_TOKEN"}, {TokenType::ERROR, "ERROR"}, {TokenType::STYLE_INCLUDE, "STYLE_INCLUDE"}, 
-        {TokenType::SCOPE_RESOLUTION, "SCOPE_RESOLUTION"}
-    };
-    auto it = typeToStringMap.find(type);
-    if (it != typeToStringMap.end()) {
-        return it->second;
-    } else {
-        return "UNKNOWN";
+    std::cout << "Token: " << typeStr;
+
+    // Print lexeme for identifiers, numbers, literals, and strings
+    if (token.type == TokenType::IDENTIFIER ||
+        token.type == TokenType::NUMBER ||
+        token.type == TokenType::FLOAT_LITERAL || // Added
+        token.type == TokenType::DOUBLE_LITERAL || // Added
+        token.type == TokenType::STRING ||
+        token.type == TokenType::STYLE_INCLUDE ||
+        token.type == TokenType::ERROR) {
+        std::cout << " (\"";
+        if (token.type == TokenType::STRING || token.type == TokenType::STYLE_INCLUDE) {
+            std::cout << std::quoted(token.lexeme);
+        } else {
+            std::cout << token.lexeme;
+        }
+         std::cout << "\")";
     }
+
+    std::cout << " at line " << token.line << ", column " << token.column << std::endl;
 }
 
 int main(int argc, char* argv[]) { 
@@ -155,12 +116,17 @@ int main(int argc, char* argv[]) {
 
     // Write tokens to file in the specified format
     for (const auto& token : tokens) {
+        // Use the shared tokenTypeToString function
         std::string typeStr = tokenTypeToString(token.type);
         outFile << typeStr;
         
         // Add lexeme if applicable, handling quotes for strings/paths
-        if (token.type == TokenType::IDENTIFIER || token.type == TokenType::NUMBER || 
-            token.type == TokenType::STRING || token.type == TokenType::STYLE_INCLUDE ||
+        if (token.type == TokenType::IDENTIFIER ||
+            token.type == TokenType::NUMBER ||
+            token.type == TokenType::FLOAT_LITERAL || // Added
+            token.type == TokenType::DOUBLE_LITERAL || // Added
+            token.type == TokenType::STRING ||
+            token.type == TokenType::STYLE_INCLUDE ||
             token.type == TokenType::ERROR) // Include lexeme for ERROR tokens too
         {
              outFile << " ";
